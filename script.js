94848f0cb30c9b0601d0dd1e57a7d0e04dcea352
Rachel Denucci <racheldenucci@yahoo.com.br>
2023-01-17 09:52:34 -0300

    done

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 function toggleMode(){
-    alert('funfou')
+    const html = document.documentElement
+
+   /* if(html.classList.contains('light')){     //if <html class="light">
+        html.classList.remove('light')
+    }
+    else{
+        html.classList.add('light')
+    }*/
+    html.classList.toggle('light')
+
+    const img = document.querySelector(".profile img")      //selecionar elemento do html (mesmo seletor do css)
+
+    if(html.classList.contains('light')){
+        img.setAttribute("src", "./images/avatar-lightmode.jpg")    //setAttribute("attribute", "novo valor")
+    }
+    else{
+        img.setAttribute("src", "https://avatars.githubusercontent.com/u/76690236?v=4")
+    }
 }
